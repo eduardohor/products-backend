07a60a8642658b76d609b90f671842e13016697b
eduado <eduardo.hor@outlook.com>
2022-12-12 15:30:59 -0300

    add middleware jwt.auth

--- routes/api.php
+++ routes/api.php
@@ -9,12 +9,13 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-Route::post('categories/store', [CategoryController::class, 'store']);
-Route::get('categories', [CategoryController::class, 'index']);
+Route::middleware('jwt.auth')->group(function () {
+  Route::get('categories', [CategoryController::class, 'index']);
+  Route::post('categories/store', [CategoryController::class, 'store']);
 
-Route::post('products/store', [ProductController::class, 'store']);
-Route::put('products/{id}', [ProductController::class, 'update']);
-Route::delete('products/{id}', [ProductController::class, 'destroy']);
-Route::get('products/{id}', [ProductController::class, 'show']);
-Route::get('products', [ProductController::class, 'index']);
-
+  Route::post('products/store', [ProductController::class, 'store']);
+  Route::put('products/{id}', [ProductController::class, 'update']);
+  Route::delete('products/{id}', [ProductController::class, 'destroy']);
+  Route::get('products/{id}', [ProductController::class, 'show']);
+  Route::get('products', [ProductController::class, 'index']);
+});
